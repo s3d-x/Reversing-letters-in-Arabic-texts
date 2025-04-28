2e957f11c1449edfb99458074c6ef3eec8e27c92
s3d-x <s.ssftps@gmail.com>
2025-04-28 18:58:06 +0300

    reshape_script.py

--- reshape_script.py
+++ reshape_script.py
@@ -1,13 +1,13 @@
-# reshape_script.py
+# reshape_script.py Discord: 4SAAD
 import arabic_reshaper
 from bidi.algorithm import get_display
 import pyperclip
 import sys
 
-# خذ النص من الحافظة (Clipboard)
+
 text = pyperclip.paste()
 
-# إعادة تشكيل الحروف
+
 reshaped_text = arabic_reshaper.reshape(text)
 bidi_text = get_display(reshaped_text)
 
